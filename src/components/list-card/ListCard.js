854b97f3270a9c6faf957079ee92f33b99d56234
Czurch <lukesferreira1217@gmail.com>
2020-02-09 18:14:45 -0500

    Final Cleanup
    -Makes code more D.R.Y.
    -Change ListCard to function component

--- src/components/list-card/ListCard.js
+++ src/components/list-card/ListCard.js
@@ -1,54 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import classNames from "classnames";
 import { Card as Base, CardHeader, CardContent } from "@material-ui/core";
 import PropTypes from "prop-types";
 import styles from "./ListCard.module.scss";
 
-class ListCard extends Component {
-  render() {
-    const { theme, cardHeader, cardItems } = this.props;
+function ListCard({ theme, cardHeader, cardItems }) {
+  const themeCheck = style =>
+    classNames(style, { [styles.dark]: theme === "dark" });
 
-    return (
-      <Base
+  return (
+    <Base
+      classes={{
+        root: themeCheck(styles.listCard)
+      }}
+    >
+      <CardHeader
+        title={cardHeader}
         classes={{
-          root: classNames(styles.listCard, { [styles.dark]: theme === "dark" })
+          root: themeCheck(styles.headerSpace),
+          title: themeCheck(styles.headerTitle)
         }}
+      />
+      <CardContent
+        classes={{
+          root: themeCheck(styles.itemSpace)
+        }}
+        component="ul"
       >
-        <CardHeader
-          title={cardHeader}
-          classes={{
-            root: classNames(styles.headerSpace, {
-              [styles.dark]: theme === "dark"
-            }),
-            title: classNames(styles.headerTitle, {
-              [styles.dark]: theme === "dark"
-            })
-          }}
-        />
-        <CardContent
-          classes={{
-            root: classNames(styles.itemSpace, {
-              [styles.dark]: theme === "dark"
-            })
-          }}
-          component="ul"
-        >
-          {cardItems.map(item => (
-            <li className={styles.listItem} key={item.key}>
-              - {item}
-            </li>
-          ))}
-        </CardContent>
-      </Base>
-    );
-  }
+        {cardItems.map(item => (
+          <li className={styles.listItem} key={item.key}>
+            - {item}
+          </li>
+        ))}
+      </CardContent>
+    </Base>
+  );
 }
 
 ListCard.propTypes = {
   cardHeader: PropTypes.string,
   cardItems: PropTypes.arrayOf(PropTypes.string)
 };
-
-ListCard.defaultProps = {};
 
 export default ListCard;
